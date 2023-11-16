1ba4e096abcf859c758e5fd1724547fa4c8b15c3
Nick Vallee <nickvallee@gmail.com>
2023-11-16 09:54:22 -0500

    add user auth

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.use(express.json());
 const port = 3000;
 
 // Models
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+UserSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
 const TaskSchema = new mongoose.Schema({
   name: String,
   title: String,
@@ -20,8 +34,34 @@ const TaskSchema = new mongoose.Schema({
   status: String
 });
 
+
 const Task = mongoose.model('Task', TaskSchema);
 
+// Auth End Points
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  await user.save();
+  const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
+  res.send({ token });
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).send('Invalid username or password');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).send('Invalid username or password');
+  }
+  const token = jwt.sign({ _id: user._id }, 'SECRET_KEY');
+  res.send({ token });
+});
+
+// Task End Points
 app.get('/tasks', (req, res) => {
   Task.find().then(tasks => res.send(tasks));
 });
@@ -31,7 +71,6 @@ app.listen(port, () => {
 });
 
 app.post('/tasks', (req, res) => {
-
   const task = new Task(req.body);
   task.save()
     .then(() => res.status(201).send(task))
@@ -71,7 +110,26 @@ app.delete('/tasks/:id', (req, res) => {
     .catch(err => res.status(400).send(err));
 });
 
+// auth middleware
+
+const auth = async (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  const data = jwt.verify(token, 'SECRET_KEY');
+  try {
+    const user = await User.findOne({ _id: data._id });
+    if (!user) {
+      throw new Error();
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Not authorized to access this resource' });
+  }
+};
+
 // Middleware for error handling
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
